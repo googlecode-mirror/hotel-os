ee6f309e1accf04f0bea0c5f13e79bccb5d1610f
thanhnhan3112 <thanhnhan3112@gmail.com>
2011-01-07 10:32:44 +0000

    +Tao gio hang(thên thanh cong ,huy chua dc)

---
+++ delcart.php
@@ -0,0 +1,21 @@
+<?php
+require('includes/application_top.php');
+	session_start();
+	$cart=$_SESSION['cart_room'];
+	$id=$_GET['room_type_id'];	
+	echo count($_SESSION['cart_room']);
+	if($id == 0)
+	{
+	 unset($_SESSION['cart_room']);
+	
+	}
+	else
+	{
+		//echo $id;		
+	  unset($_SESSION['cart_room'][$id]);
+	 //echo "gfgf";
+	}
+	//header("location:cart.php");
+	//tep_redirect(tep_href_link("cart.php"));
+	exit();
+?>
